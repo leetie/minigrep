4990129e63e206bb1f5f30a232e4632fbadfd4b6
Jesse Parsons <savvyjesse@aol.com>
2021-01-25 21:32:01 -0500

    add some fns

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use std::env;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing the arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    run(config);
+}
 
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    println!("With text:\n{}", contents);
 }
