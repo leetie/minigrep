5119479203fe977b0667ba0e682fdf45d8d7a20b
Jesse Parsons <savvyjesse@aol.com>
2021-01-27 19:51:26 -0500

    change error msg to stderr

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error: {}", err);
+        eprintln!("Error: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Error: {}", e);
+        eprintln!("Error: {}", e);
         process::exit(1);
     }
 }
