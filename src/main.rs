5dc6020a536b30997e5169eb2bf5ec6d99c04f8d
Jesse Parsons <savvyjesse@aol.com>
2021-01-27 19:47:31 -0500

    add case insensitive search

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
         println!("Error: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Error: {}", e);
