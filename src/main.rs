ce1cc3dc7aa825194a3a9ddef62d5e64ff150e6a
Jesse Parsons <savvyjesse@aol.com>
2021-01-27 19:08:05 -0500

    modularize

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
-use std::{env, fs, process};
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {}", err);
+        println!("Error: {}", err);
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    run(config);
-}
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Error: {}", e);
+        process::exit(1);
     }
-}
-
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
 }
